4c39c964f9857f813feff84905f53e9bb851ef4c
DESKTOP-BNLC6QK\damtrongan <damtrongan@gmail.com>
2025-09-03 09:24:14 +0700

    test subnet

--- main.tf
+++ main.tf
@@ -7,3 +7,24 @@ data "aws_vpc" "dev-vpc" {
 output "aws_vpc_id" {
   value = data.aws_vpc.dev-vpc.id
 }
+
+data "aws_subnet" "public_subnet" {
+  filter {
+    name   = "tag:Name"
+    values = ["andt01-subnet-public1-ap-northeast-2a"]
+  }
+}
+
+output "aws_public_subnet_id" {
+  value = data.aws_subnet.public_subnet.id
+}
+
+resource "aws_instance" "bastion" {
+  ami = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "MyInstance"
+  }
+}
